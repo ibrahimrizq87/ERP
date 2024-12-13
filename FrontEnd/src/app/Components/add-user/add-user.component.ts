7e91aed222b2a5b1acda8327989a380e503a5d7e
marinasamir15 <marinasamir15102001@gmail.com>
2024-12-13 16:58:34 +0200

    add users

--- FrontEnd/src/app/Components/add-user/add-user.component.ts
+++ FrontEnd/src/app/Components/add-user/add-user.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../shared/services/user.service';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
@@ -12,32 +12,63 @@ import { CommonModule } from '@angular/common';
   styleUrl: './add-user.component.css'
 })
 export class AddUserComponent {
-  msgError: string = '';
+  msgError: any[] = [];
   isLoading: boolean = false;
-
+  readonly maxImageSize = 2048 * 1024;
   constructor(private _UserService:UserService , private _Router: Router) {
    
   }
-
+  // 'name' => 'required|string|max:255',
+  // 'username' => 'required|string|max:255|unique:users,username',
+  // 'password' => 'required|string|min:8',
+  // 'role' => 'required|in:accountant,worker,admin',
+  // 'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
 
   userForm: FormGroup = new FormGroup({
-    user_name: new FormControl(null, [Validators.required]),
-    password: new FormControl(null, [Validators.required]),
+    name:new FormControl(null, [Validators.required,Validators.maxLength(255)]),
+    username: new FormControl(null, [Validators.required,Validators.maxLength(255)]),
+    password: new FormControl(null, [Validators.required,Validators.minLength(8)]),
     role: new FormControl(null, [Validators.required]),
+    image: new FormControl(null, [this.validateImage.bind(this)]),
   });
+  selectedFile: File | null = null;
 
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      this.userForm.patchValue({ image: file });
+    }
+  }
+
+  validateImage(control: AbstractControl): ValidationErrors | null {
+    const file = this.selectedFile;
+    if (file) {
+      const fileType = file.type;
+      const fileSize = file.size;
+      const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/gif'];
+      if (!allowedTypes.includes(fileType)) {
+        return { invalidFileType: true };
+      }
+      if (fileSize > this.maxImageSize) {
+        return { fileTooLarge: true };
+      }
+    }
+    return null;
+  }
   handleForm() {
    
     if (this.userForm.valid) {
       this.isLoading = true;
 
       const formData = new FormData();
-    
-      formData.append('user_name', this.userForm.get('user_name')?.value);
+      formData.append('name', this.userForm.get('name')?.value);
+      formData.append('username', this.userForm.get('username')?.value);
       formData.append('password', this.userForm.get('password')?.value);
       formData.append('role', this.userForm.get('role')?.value);
-
-
+      if (this.selectedFile) {
+        formData.append('image', this.selectedFile);
+      }
       this._UserService.addUser(formData).subscribe({
         next: (response) => {
           console.log(response);
@@ -48,9 +79,21 @@ export class AddUserComponent {
         },
         error: (err: HttpErrorResponse) => {
           this.isLoading = false;
-           this.msgError = err.error.error;
-          console.log(err);
-        }
+          this.msgError = [];
+      
+          if (err.error && err.error.errors) {
+             
+              for (const key in err.error.errors) {
+                  if (err.error.errors[key] instanceof Array) {
+                      this.msgError.push(...err.error.errors[key]);
+                  } else {
+                      this.msgError.push(err.error.errors[key]);
+                  }
+              }
+          }
+      
+          console.error(this.msgError); 
+      },
       });
     }
   }
