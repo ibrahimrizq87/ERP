7cc666eb9f64444a95a665870a81d79e6f487eec
marinasamir15 <marinasamir15102001@gmail.com>
2024-12-14 19:34:45 +0200

    add update ,Show Machine

--- FrontEnd/src/app/Components/show-machine/show-machine.component.ts
+++ FrontEnd/src/app/Components/show-machine/show-machine.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MachineService } from '../../shared/services/machine.service';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-show-machine',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './show-machine.component.html',
   styleUrl: './show-machine.component.css'
 })
-export class ShowMachineComponent {
+export class ShowMachineComponent implements OnInit {
 
+  machineData: any;
+
+  constructor(
+    private _MachineService: MachineService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const machineId = this.route.snapshot.paramMap.get('id');
+    if (machineId) {
+      this.fetchMachineData(machineId);
+    }
+  }
+
+ 
+  fetchMachineData(machineId: string): void {
+    this._MachineService.getMachineById(machineId).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.machineData = response|| {};
+      },
+      error: (err: HttpErrorResponse) => {
+        console.error('Error fetching Product data:', err.message);
+      }
+    });
+  }
+  
 }
--- FrontEnd/src/app/Components/update-machine/update-machine.component.ts
+++ FrontEnd/src/app/Components/update-machine/update-machine.component.ts
@@ -1,11 +1,107 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../shared/services/product.service';
+import { MachineService } from '../../shared/services/machine.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-update-machine',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './update-machine.component.html',
   styleUrl: './update-machine.component.css'
 })
-export class UpdateMachineComponent {
+export class UpdateMachineComponent implements OnInit {
+  msgError: any[] = [];
+  isLoading: boolean = false;
+  products:any[]=[];
+  machineForm: FormGroup;;
+    constructor(
+    private fb: FormBuilder,
+    private router: Router, 
+    private _ProductService:ProductService,
+    private _MachineService:MachineService ,
+    private route: ActivatedRoute
+  ) {
+   
+    this.machineForm = new FormGroup({
+      machine_number: this.fb.control(null, [Validators.required,Validators.maxLength(255)]),
+      product_id: this.fb.control(null, [Validators.required]),
+      
+    });
+  }
+ 
+    ngOnInit(): void {
+    const machineId = this.route.snapshot.paramMap.get('id'); 
+    if (machineId) {
+      this.fetchMachineData(machineId);
+    }
+    this.loadProducts();
+  }
+  fetchMachineData(machineId: string): void {
+    this._MachineService.getMachineById(machineId).subscribe({
+      next: (response) => {
+        if (response) {
+          const machineData = response ; 
+          console.log(machineData)
+          this.machineForm.patchValue({
+            machine_number:machineData.machine_number,
+            product_id:machineData.product.id,
+              
+          });
+       
+        }
+      },
+      error: (err: HttpErrorResponse) => {
+        this.msgError = err.error.error;
+      }
+    });
+  }
+  loadProducts(): void {
+    this._ProductService.viewAllProducts().subscribe({
+      next: (response) => {
+        if (response) {
+          console.log(response);
+          this.products = response; 
+        
+        }
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+ 
 
+  
+  handleForm() {
+    if (this.machineForm.valid) {
+      this.isLoading = true;
+      const formData = new FormData();
+
+      formData.append('machine_number', this.machineForm.get('machine_number')?.value);
+      formData.append('product_id', this.machineForm.get('product_id')?.value);
+ 
+      const machineId = this.route.snapshot.paramMap.get('id');
+      if (machineId){
+      this._MachineService.updateMachine(machineId,formData).subscribe({
+        next: (response) => {
+          this.isLoading = false;
+          if (response) {
+            this.router.navigate(['/dashboard/machines']);
+          }
+        },
+        error: (err: HttpErrorResponse) => {
+          this.isLoading = false;
+          this.msgError = err.error.error;
+        }
+      });
+    }
+  }}
+  onCancel(): void {
+        this.machineForm.reset();
+       
+        this.router.navigate(['/dashboard/machines']); 
+      }  
 }
