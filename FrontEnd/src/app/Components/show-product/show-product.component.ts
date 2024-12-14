b0985a5e9ca5add37d8f4a872a05316b9dbb8291
marinasamir15 <marinasamir15102001@gmail.com>
2024-12-14 18:55:05 +0200

    update,show product

--- FrontEnd/src/app/Components/show-product/show-product.component.ts
+++ FrontEnd/src/app/Components/show-product/show-product.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../shared/services/product.service';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-show-product',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './show-product.component.html',
   styleUrl: './show-product.component.css'
 })
-export class ShowProductComponent {
+export class ShowProductComponent implements OnInit {
 
+  productData: any;
+
+  constructor(
+    private _ProductService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id');
+    if (productId) {
+      this.fetchProductData(productId);
+    }
+  }
+
+ 
+  fetchProductData(productId: string): void {
+    this._ProductService.getProductById(productId).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.productData = response|| {};
+      },
+      error: (err: HttpErrorResponse) => {
+        console.error('Error fetching Product data:', err.message);
+      }
+    });
+  }
+  
 }
--- FrontEnd/src/app/Components/update-product/update-product.component.ts
+++ FrontEnd/src/app/Components/update-product/update-product.component.ts
@@ -1,11 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../shared/services/product.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-update-product',
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './update-product.component.html',
   styleUrl: './update-product.component.css'
 })
-export class UpdateProductComponent {
+export class UpdateProductComponent implements OnInit {
+  msgError: any[] = [];
+  isLoading: boolean = false;
 
+  productForm: FormGroup;;
+    constructor(
+    private fb: FormBuilder,
+    private router: Router, 
+    private _ProductService:ProductService ,
+    private route: ActivatedRoute
+  ) {
+   
+    this.productForm = new FormGroup({
+      name: this.fb.control(null, [Validators.required,Validators.maxLength(255)]),
+      price: this.fb.control(null, [Validators.required,Validators.min(0)]),
+      amount: this.fb.control(null, [Validators.required,Validators.min(0)]),   
+    });
+  }
+ 
+    ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id'); 
+    if (productId) {
+      this.fetchProductData(productId);
+    }
+  }
+  fetchProductData(productId: string): void {
+    this._ProductService.getProductById(productId).subscribe({
+      next: (response) => {
+        if (response) {
+          const productData = response ; 
+          console.log(productData)
+          this.productForm.patchValue({
+            name:productData.name,
+            price:productData.price,
+            amount:productData.amount,
+            
+           
+           
+          });
+       
+        }
+      },
+      error: (err: HttpErrorResponse) => {
+        this.msgError = err.error.error;
+      }
+    });
+  }
+  
+ 
+
+  
+  handleForm() {
+    if (this.productForm.valid) {
+      this.isLoading = true;
+      const formData = new FormData();
+
+      formData.append('name', this.productForm.get('name')?.value);
+      formData.append('price', this.productForm.get('price')?.value);
+      formData.append('amount', this.productForm.get('amount')?.value);
+     
+     
+      const productId = this.route.snapshot.paramMap.get('id');
+      if (productId){
+      this._ProductService.updateProduct(productId,formData).subscribe({
+        next: (response) => {
+          this.isLoading = false;
+          if (response) {
+            this.router.navigate(['/dashboard/products']);
+          }
+        },
+        error: (err: HttpErrorResponse) => {
+          this.isLoading = false;
+          this.msgError = err.error.error;
+        }
+      });
+    }
+  }}
+  onCancel(): void {
+        this.productForm.reset();
+       
+        this.router.navigate(['/dashboard/products']); 
+      }  
 }
