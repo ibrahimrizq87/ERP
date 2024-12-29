cce7c9e2a958b9bb216a7d08f3ea4f2acffec0b9
marinasamir15 <marinasamir15102001@gmail.com>
2024-12-29 21:26:28 +0200

    add show purchase

--- FrontEnd/src/app/Components/show-purchase-invoices/show-purchase-invoices.component.ts
+++ FrontEnd/src/app/Components/show-purchase-invoices/show-purchase-invoices.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { InvoiceService } from '../../shared/services/invoice.service';
+
 
 @Component({
   selector: 'app-show-purchase-invoices',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './show-purchase-invoices.component.html',
   styleUrl: './show-purchase-invoices.component.css'
 })
-export class ShowPurchaseInvoicesComponent {
+export class ShowPurchaseInvoicesComponent implements OnInit {
 
+  purchaseData: any;
+
+  constructor(
+    private _InvoiceService:InvoiceService ,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const purchaseId = this.route.snapshot.paramMap.get('id');
+    if (purchaseId) {
+      this.fetchPurchaseData(purchaseId);
+    }
+  }
+  get totalAfterTax(): number {
+    const totalCash = parseFloat(this.purchaseData?.total_cash || '0');
+    const taxAmount = parseFloat(this.purchaseData?.tax_amount || '0');
+    return totalCash - taxAmount;
+  }
+ 
+  fetchPurchaseData (purchaseId: string): void {
+    this._InvoiceService.getPurchaseInvoiceById(purchaseId).subscribe({
+      next: (response) => {
+        console.log(response.data);
+        this.purchaseData = response.data|| {};
+      },
+      error: (err: HttpErrorResponse) => {
+        console.error('Error fetching Purchase data:', err.message);
+      }
+    });
+  }
+  
 }
