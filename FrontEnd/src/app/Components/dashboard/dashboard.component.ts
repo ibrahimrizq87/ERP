b85860b3a7f121477f8d8c7c611cb5f52606a11e
marinasamir15 <marinasamir15102001@gmail.com>
2024-12-31 19:25:28 +0200

    add changes

--- FrontEnd/src/app/Components/dashboard/dashboard.component.ts
+++ FrontEnd/src/app/Components/dashboard/dashboard.component.ts
@@ -13,6 +13,8 @@ import { AccountingService } from '../../shared/services/accounts.service';
 })
 export class DashboardComponent {
   dropdownStates: { [key: string]: boolean } = {};
+  renderer: any;
+  el: any;
   constructor (private router :Router , private _AccountingService:AccountingService ){}
  accounts:any;
   toggleDropdown(navItem: string) {
@@ -25,13 +27,12 @@ export class DashboardComponent {
     this.getParentAccounts(); 
   }
 
-  getParentAccounts(){
+  getParentAccounts() {
     this._AccountingService.getParentAccounts().subscribe({
       next: (response) => {
         if (response) {
           this.accounts = response.data; 
-          console.log(this.accounts);
-
+          console.log( this.accounts)
         }
       },
       error: (err) => {
@@ -39,4 +40,29 @@ export class DashboardComponent {
       }
     });
   }
+
+  // Recursive function to render accounts and their children
+  renderAccountLinks(account: any): string {
+    if (account.children && account.children.length > 0) {
+      return `
+        <li class="nav-item nested">
+          <div class="d-flex justify-content-between align-items-center" (click)="toggleDropdown('account_${account.id}')">
+            <span>{{ account.account_name }}</span>
+            <i [ngClass]="isDropdownOpen('account_${account.id}') ? 'fa-angle-down' : 'fa-chevron-left'" class="fa-solid"></i>
+          </div>
+          <ul class="nav flex-column ms-3" *ngIf="isDropdownOpen('account_${account.id}')">
+            <ng-container *ngFor="let child of account.children">
+              <li class="nav-item">
+                <a class="nav-link" routerLink="accounting/{{child.id}}">{{ child.account_name }}</a>
+              </li>
+            </ng-container>
+          </ul>
+        </li>
+      `;
+    } else {
+      return `<li class="nav-item nested"><a class="nav-link" routerLink="accounting/{{account.id}}">{{account.account_name}}</a></li>`;
+    }
+  }
+
+
 }
