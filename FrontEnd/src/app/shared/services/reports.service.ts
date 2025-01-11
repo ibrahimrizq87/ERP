14098e912a19494eb0bb392c7086dff1eaf6a658
marinasamir15 <marinasamir15102001@gmail.com>
2025-01-11 13:27:50 +0200

    add show equation

---
+++ FrontEnd/src/app/shared/services/reports.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportsService {
+
+  private baseURL = environment.apiUrl;
+  constructor(private _HttpClient: HttpClient) { 
+  }
+ 
+  yearReport(): Observable<any> {
+    const token = localStorage.getItem('Gtoken');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this._HttpClient.get(`${this.baseURL}/accounts/get-reports`,{ headers })
+  }
+}
