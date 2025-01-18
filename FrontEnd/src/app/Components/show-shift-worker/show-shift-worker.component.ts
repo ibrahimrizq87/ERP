26446c2a1a01ef4748f6aeae2ba831e4f290e25c
marinasamir15 <marinasamir15102001@gmail.com>
2025-01-18 12:26:04 +0200

    add component

---
+++ FrontEnd/src/app/Components/show-shift-worker/show-shift-worker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-shift-worker',
+  imports: [],
+  templateUrl: './show-shift-worker.component.html',
+  styleUrl: './show-shift-worker.component.css'
+})
+export class ShowShiftWorkerComponent {
+
+}
---
+++ FrontEnd/src/app/Components/update-shift-worker/update-shift-worker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-shift-worker',
+  imports: [],
+  templateUrl: './update-shift-worker.component.html',
+  styleUrl: './update-shift-worker.component.css'
+})
+export class UpdateShiftWorkerComponent {
+
+}
