3fe3b7a361cfb736dfdb6309584d8200fff80d42
marinasamir15 <marinasamir15102001@gmail.com>
2025-01-24 15:22:42 +0200

    add changes

---
+++ FrontEnd/src/app/Components/expenses-report/expenses-report.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expenses-report',
+  imports: [],
+  templateUrl: './expenses-report.component.html',
+  styleUrl: './expenses-report.component.css'
+})
+export class ExpensesReportComponent {
+
+}
