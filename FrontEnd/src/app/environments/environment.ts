98644c2d06d004c42ba0646d1ab3264c73aaffc5
ibrahimrizq87 <ibrahimrizqabd@gmail.com>
2025-01-31 14:34:39 +0200

    b

--- FrontEnd/src/app/environments/environment.ts
+++ FrontEnd/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    // apiUrl: 'http://data.dous.in/api' 
-    apiUrl: 'http://127.0.0.1:8000/api' 
+    apiUrl: 'http://data.dous.in/api' 
+    // apiUrl: 'http://127.0.0.1:8000/api' 
 
   };
